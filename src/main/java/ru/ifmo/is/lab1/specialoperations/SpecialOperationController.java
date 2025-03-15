3b229ea1d1fd21d3f45d49b2be14a8c7290b12f0
Егор Деревягин <egord1010@mail.ru>
2025-03-15 14:17:56 +0700

    Special Operation Monetization

--- src/main/java/ru/ifmo/is/lab1/specialoperations/SpecialOperationController.java
+++ src/main/java/ru/ifmo/is/lab1/specialoperations/SpecialOperationController.java
@@ -15,8 +15,9 @@ import ru.ifmo.is.lab1.common.search.SearchDto;
 import ru.ifmo.is.lab1.applications.LabWorkService;
 import ru.ifmo.is.lab1.applications.dto.LabWorkCreateDto;
 import ru.ifmo.is.lab1.applications.dto.LabWorkDto;
-import ru.ifmo.is.lab1.people.PersonService;
-import ru.ifmo.is.lab1.people.dto.PersonDto;
+import ru.ifmo.is.lab1.monetization.MonetizationService;
+import ru.ifmo.is.lab1.monetization.dto.MonetizationCreateDto;
+import ru.ifmo.is.lab1.monetization.dto.MonetizationDto;
 
 import java.util.Collections;
 
@@ -29,58 +30,67 @@ public class SpecialOperationController {
 
   private final SpecialOperationService service;
   private final LabWorkService labWorkService;
-  private final PersonService personService;
+  private final MonetizationService monetizationService;
 
-  @GetMapping("/filter-by-minimalPoint")
-  @Operation(summary = "Вернуть массив объектов, значение поля `minimalpoint` которых меньше заданного.")
-  public ResponseEntity<Page<LabWorkDto>> filterLabWorksByMinimalPoint(
-    @RequestParam("minimalPoint") Integer minimalPoint,
-    @PageableDefault(size = 20) Pageable pageable
-  ) {
-    var searchCriteria = SearchCriteria.builder().filterKey("minimalPoint").operation("LT").value(minimalPoint).build();
-    var searchDto = SearchDto.builder()
-      .dataOption("ALL")
-      .searchCriteriaList(Collections.singletonList(searchCriteria))
-      .build();
+//  @GetMapping("/filter-by-minimalPoint")
+//  @Operation(summary = "Вернуть массив объектов, значение поля `minimalpoint` которых меньше заданного.")
+//  public ResponseEntity<Page<LabWorkDto>> filterLabWorksByMinimalPoint(
+//    @RequestParam("minimalPoint") Integer minimalPoint,
+//    @PageableDefault(size = 20) Pageable pageable
+//  ) {
+//    var searchCriteria = SearchCriteria.builder().filterKey("minimalPoint").operation("LT").value(minimalPoint).build();
+//    var searchDto = SearchDto.builder()
+//      .dataOption("ALL")
+//      .searchCriteriaList(Collections.singletonList(searchCriteria))
+//      .build();
+//
+//    var labworks = labWorkService.findBySearchCriteria(searchDto, pageable);
+//    return ResponseEntity.ok()
+//      .header("X-Total-Count", String.valueOf(labworks.getTotalElements()))
+//      .body(labworks);
+//  }
 
-    var labworks = labWorkService.findBySearchCriteria(searchDto, pageable);
-    return ResponseEntity.ok()
-      .header("X-Total-Count", String.valueOf(labworks.getTotalElements()))
-      .body(labworks);
-  }
+//  @GetMapping("/filter-by-author")
+//  @Operation(summary = "Вернуть массив объектов, значение поля `author` которых больше заданного.")
+//  public ResponseEntity<Page<LabWorkDto>> filterLabWorksByAuthor(
+//    @RequestParam("author_id") Integer author,
+//    @PageableDefault(size = 20) Pageable pageable
+//  ) {
+//    var searchCriteria = SearchCriteria.builder().filterKey("author_id").operation("GT").value(author).build();
+//    var searchDto = SearchDto.builder()
+//      .dataOption("ALL")
+//      .searchCriteriaList(Collections.singletonList(searchCriteria))
+//      .build();
+//
+//    var labworks = labWorkService.findBySearchCriteria(searchDto, pageable);
+//    return ResponseEntity.ok()
+//      .header("X-Total-Count", String.valueOf(labworks.getTotalElements()))
+//      .body(labworks);
+//  }
 
-  @GetMapping("/filter-by-author")
-  @Operation(summary = "Вернуть массив объектов, значение поля `author` которых больше заданного.")
-  public ResponseEntity<Page<LabWorkDto>> filterLabWorksByAuthor(
-    @RequestParam("author_id") Integer author,
-    @PageableDefault(size = 20) Pageable pageable
-  ) {
-    var searchCriteria = SearchCriteria.builder().filterKey("author_id").operation("GT").value(author).build();
-    var searchDto = SearchDto.builder()
-      .dataOption("ALL")
-      .searchCriteriaList(Collections.singletonList(searchCriteria))
-      .build();
+//  @PostMapping("/add-labwork-to-discipline/{id}")
+//  @Operation(summary = "Добавить лабораторную работу в программу дисциплины")
+//  public ResponseEntity<LabWorkDto> addLabWorkToDiscipline(@PathVariable int id, @Valid @RequestBody LabWorkCreateDto dto) {
+//    var labwork = service.addLabWorkToDiscipline(id, dto);
+//    return ResponseEntity.ok(labwork);
+//  }
 
-    var labworks = labWorkService.findBySearchCriteria(searchDto, pageable);
-    return ResponseEntity.ok()
-      .header("X-Total-Count", String.valueOf(labworks.getTotalElements()))
-      .body(labworks);
-  }
+//  @GetMapping("/get-uniq-authors")
+//  @Operation(summary = "Вернуть массив уникальных значений поля 'author' по всем объектам")
+//  public ResponseEntity<Page<PersonDto>> getUniqAuthors(@PageableDefault(size = 20) Pageable pageable) {
+//
+//    var persons = personService.getAll(pageable);
+//    return ResponseEntity.ok()
+//            .header("X-Total-Count", String.valueOf(persons.getTotalElements()))
+//            .body(persons);
+//  }
 
-  @PostMapping("/add-labwork-to-discipline/{id}")
-  @Operation(summary = "Добавить лабораторную работу в программу дисциплины")
-  public ResponseEntity<LabWorkDto> addLabWorkToDiscipline(@PathVariable int id, @Valid @RequestBody LabWorkCreateDto dto) {
-    var labwork = service.addLabWorkToDiscipline(id, dto);
-    return ResponseEntity.ok(labwork);
-  }
 
-  @GetMapping("/get-uniq-authors")
-  @Operation(summary = "Вернуть массив уникальных значений поля 'author' по всем объектам")
-  public ResponseEntity<Page<PersonDto>> getUniqAuthors(@PageableDefault(size = 20) Pageable pageable) {
 
-    var persons = personService.getAll(pageable);
-    return ResponseEntity.ok()
-            .header("X-Total-Count", String.valueOf(persons.getTotalElements()))
-            .body(persons);
+  @PostMapping("/add-monetization-to-application/{sum}")
+  @Operation(summary = "Добавить монетизацию приложению")
+  public ResponseEntity<MonetizationDto> addMonetizationToApplication(@PathVariable float sum, @Valid @RequestBody MonetizationCreateDto dto) {
+    var monetization = service.addMonetizationToApplication(sum, dto);
+    return ResponseEntity.ok(monetization);
   }
 }
--- src/main/java/ru/ifmo/is/lab1/specialoperations/SpecialOperationService.java
+++ src/main/java/ru/ifmo/is/lab1/specialoperations/SpecialOperationService.java
@@ -7,19 +7,33 @@ import ru.ifmo.is.lab1.applications.LabWorkRepository;
 import ru.ifmo.is.lab1.applications.LabWorkService;
 import ru.ifmo.is.lab1.applications.dto.LabWorkCreateDto;
 import ru.ifmo.is.lab1.applications.dto.LabWorkDto;
+import ru.ifmo.is.lab1.monetization.MonetizationRepository;
+import ru.ifmo.is.lab1.monetization.MonetizationService;
+import ru.ifmo.is.lab1.monetization.dto.MonetizationCreateDto;
+import ru.ifmo.is.lab1.monetization.dto.MonetizationDto;
+import ru.ifmo.is.lab1.monetization.mappers.MonetizationMapper;
 
 
 @Service
 @RequiredArgsConstructor
 public class SpecialOperationService {
 
-  private final LabWorkRepository repository;
-  private final LabWorkService labWorkService;
-  private final LabWorkMapper mapper;
+//  private final LabWorkRepository repository;
+//  private final LabWorkService labWorkService;
+//  private final LabWorkMapper mapper;
 
-  public LabWorkDto addLabWorkToDiscipline(int id, LabWorkCreateDto dto) {
-    dto.setDisciplineId(id);
-    return labWorkService.create(dto);
+  private final MonetizationRepository repository;
+  private final MonetizationService monetizationService;
+  private final MonetizationMapper mapper;
+
+//  public LabWorkDto addLabWorkToDiscipline(int id, LabWorkCreateDto dto) {
+//    dto.setDisciplineId(id);
+//    return labWorkService.create(dto);
+//  }
+
+  public MonetizationDto addMonetizationToApplication(float sum, MonetizationCreateDto dto) {
+    dto.setSum(sum);
+    return monetizationService.create(dto);
   }
 
 }
